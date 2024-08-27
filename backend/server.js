d92050c1fdf80e4be6bcf28ebc1a2bfcb996b4d3
Zeffun <84435005+Zeffun@users.noreply.github.com>
2024-08-27 15:44:29 +0800

    feat: test render

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
+app.use(express.static("../frontend/dist"));
+
+app.get("/index.html", (req, res) => {
   res.send("Hello World!");
 });
 
